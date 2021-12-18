754999cac0feeba503b7df046b56093840c131d7
Rafael Shintani <rhiroshi14@hotmail.com>
2021-12-18 08:54:34 -0300

    Configurado i18n

--- config/application.rb
+++ config/application.rb
@@ -24,17 +24,10 @@ module EcommerceObc
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :'pt-BR' # Configura o locale padrão como pt-BR
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{.rb, .yml}')] # Adiciona locais para procurar os locales
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
   end
 end
