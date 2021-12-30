47bb5e2bb0a8a29adb226c45d297f669a132453f
Rafael Shintani <rhiroshi14@hotmail.com>
2021-12-30 19:15:51 -0300

    Adicionado imagem ao produto e o teste de presença da imagem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
   validates :name, uniqueness: { case_sensitive: false }
-  validates :name, :price, :description, presence: true
+  validates :image, :name, :price, :description, presence: true
   validates :price, numericality: { greater_than: 0 }
+
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
+
+  has_one_attached :image
 end
