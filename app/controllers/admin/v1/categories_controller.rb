92566c8dafe16b37fa13279aaea376e4e02c6253
Rafael H. F. Shintani <rhiroshi14@hotmail.com>
2022-01-12 12:59:49 -0300

    Adicionado remoção de category

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,11 +1,12 @@
 module Admin::V1
   class CategoriesController < ApiController
+    before_action :load_category, only: [:update, :destroy]
+
     def index
       @categories = Category.all
     end
 
     def update
-      @category = Category.find(params[:id])
       @category.attributes = category_params
       save_category!
     end
@@ -16,7 +17,17 @@ module Admin::V1
       save_category!
     end
 
+    def destroy
+      @category.destroy
+    rescue
+      render_error(fields: @category.errors.messages)
+    end
+
     private
+
+    def load_category
+      @category = Category.find(params[:id])
+    end
 
     def category_params
       return {} unless params.has_key?(:category)
