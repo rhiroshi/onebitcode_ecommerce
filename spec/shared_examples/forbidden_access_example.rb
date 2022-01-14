44be72a693faab2b760e866e1a44973f9988c41f
Rafael H. F. Shintani <rhiroshi14@hotmail.com>
2022-01-14 13:41:58 -0300

    Adicionado shared_examples

---
+++ spec/shared_examples/forbidden_access_example.rb
@@ -0,0 +1,9 @@
+shared_examples "forbidden_access" do
+  it "returns error message" do
+    expect(body_json['errors']['messages']).to eq "Forbidden access"
+  end
+
+  it "Retruns forbidden status" do
+    expect(response).to have_http_status(:forbidden)
+  end
+end
