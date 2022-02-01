3845b7f43b788e313871c8540496560597045dce
Rafael H. F. Shintani <rhiroshi14@hotmail.com>
2022-02-01 12:51:39 -0300

    Adicionado status ao product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
   validates :name, uniqueness: { case_sensitive: false }
-  validates :image, :name, :price, :description, presence: true
+  validates :status, :image, :name, :price, :description, presence: true
   validates :price, numericality: { greater_than: 0 }
 
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
+
+  enum status: {available: 1, unavailable: 2}
 
   has_one_attached :image
   include NameSearchable
