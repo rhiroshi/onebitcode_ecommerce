f1acfa13338014492218cb21fe2ba4eab51370e2
Rafael Shintani <rhiroshi14@hotmail.com>
2022-02-08 22:04:25 -0300

    Finalizado ProductSavingService

--- app/services/admin/product_saving_service.rb
+++ app/services/admin/product_saving_service.rb
@@ -5,7 +5,40 @@ module Admin
     attr_reader :product, :errors
 
     def initialize(params, product=nil)
-      
+      params = params.deep_symbolize_keys
+      @product_params = params.reject { |k| k == :productable_attributes }
+      @productable_params = params[:productable_attributes] || {}
+      @errors = {}
+      @product = product || Product.new
+    end
+
+    def call
+      Product.transaction do
+        @product.attributes = @product_params.reject { |k| k == :productable }
+        build_productable
+      ensure
+        save!
+      end
+
+    end
+
+    def build_productable
+      @product.productable ||= @product_params[:productable ].camelcase.safe_constantize.new
+      @product.productable.attributes = @productable_params
+    end
+
+    def save!
+      save_record!(@product.productable) if @product.productable.present?
+      save_record!(@product)
+      raise NotSavedProductError if @errors.present?
+    rescue => e
+      raise NotSavedProductError
+    end
+
+    def save_record!(record)
+      record.save!
+    rescue ActiveRecord::RecordInvalid
+      @errors.merge!(record.errors.messages)
     end
   end
 end
