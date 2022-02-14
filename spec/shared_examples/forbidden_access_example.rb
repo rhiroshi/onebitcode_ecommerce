ea68806fe77f8a5fe46f28ddafb2c9d958028c64
Rafael H. F. Shintani <rhiroshi14@hotmail.com>
2022-02-14 13:00:07 -0300

    Fix shared example forbidden_access

--- spec/shared_examples/forbidden_access_example.rb
+++ spec/shared_examples/forbidden_access_example.rb
@@ -1,4 +1,4 @@
-shared_examples "forbidden_access" do
+shared_examples "forbidden access" do
   it "returns error message" do
     expect(body_json['errors']['message']).to eq "Forbidden access"
   end
