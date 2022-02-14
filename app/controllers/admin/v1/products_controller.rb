d50c03997e9bca02428dd775b341f9c7d0cea782
Rafael H. F. Shintani <rhiroshi14@hotmail.com>
2022-02-14 12:51:46 -0300

    cria show do products

--- app/controllers/admin/v1/products_controller.rb
+++ app/controllers/admin/v1/products_controller.rb
@@ -1,18 +1,30 @@
 module Admin::V1
   class ProductsController < ApiController
+    before_action :load_product, only: %i(show update destroy)
+
     def index
       @products = load_products
     end
 
     def create
+      run_service
+    rescue Admin::ProductSavingService::NotSavedProductError
+      render_error(fields: @saving_service.errors)
     end
 
     def show; end
 
     def update
+      run_service
+    rescue Admin::ProductSavingService::NotSavedProductError
+      render_error(fields: @saving_service.errors)
     end
 
     def destroy
+      @product.productable.destroy!
+      @product.destroy!
+    rescue ActiveRecord::RecordNotDestroyed
+      render_error(fields: @product.errors.messages.merge(@product.productable.errors.messages))
     end
 
     private
@@ -21,9 +33,13 @@ module Admin::V1
       Admin::ModelLoadingService.new(Product.all, permitted).call
     end
 
+    def load_product
+      @product = Product.find(params[:id])
+    end
+
     def product_params
       return {} unless params.has_key?(:product)
-      permitted_params = params.permit(:product).permit(:id, :name, :description, :image, :price, :productable, :status, category_ids: [])
+      permitted_params = params.required(:product).permit(:id, :name, :description, :image, :price, :productable, :status, category_ids: [])
 
       permitted_params.merge(productable_params)
     end
@@ -31,7 +47,8 @@ module Admin::V1
     def productable_params
       productable_type = params[:product][:productable] || @product&.productable_type&.underscore
       return unless productable_type.present?
-      send("#{productable_type}_params")
+      productable_attributes = send("#{productable_type}_params")
+      {productable_attributes: productable_attributes}
     end
 
     def game_params
