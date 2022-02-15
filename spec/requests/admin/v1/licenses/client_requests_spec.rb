d877ad6f53aedbbdcf4f975a2e78ba09785a4c48
Rafael H. F. Shintani <rhiroshi14@hotmail.com>
2022-02-15 14:29:06 -0300

    Fix testes de licensa²

--- spec/requests/admin/v1/licenses/client_requests_spec.rb
+++ spec/requests/admin/v1/licenses/client_requests_spec.rb
@@ -1,46 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe "Admin V1 Products as :client", type: :request do
+RSpec.describe "Admin V1 Licenses as :client", type: :request do
   let(:user) { create(:user, profile: :client) }
 
-  context "GET /products" do
-    let(:url) { "/admin/v1/products" }
-    let!(:products) { create_list(:product, 5) }
+  context "GET /licenses" do
+    let(:url) { "/admin/v1/licenses" }
+    let!(:licenses) { create_list(:license, 10) }
 
     before(:each) { get url, headers: auth_header(user) }
 
     include_examples "forbidden access"
   end
 
-  context "POST /products" do
-    let(:url) { "/admin/v1/products" }
+  context "POST /licenses" do
+    let(:url) { "/admin/v1/licenses" }
 
     before(:each) { post url, headers: auth_header(user) }
 
     include_examples "forbidden access"
   end
 
-  context "GET /products/:id" do
-    let(:product) { create(:product) }
-    let(:url) { "/admin/v1/products/#{product.id}" }
+  context "GET /licenses/:id" do
+    let(:license) { create(:license) }
+    let(:url) { "/admin/v1/licenses/#{license.id}" }
 
     before(:each) { get url, headers: auth_header(user) }
 
     include_examples "forbidden access"
   end
 
-  context "PATCH /products/:id" do
-    let(:product) { create(:product) }
-    let(:url) { "/admin/v1/products/#{product.id}" }
+  context "PATCH /licenses/:id" do
+    let(:license) { create(:license) }
+    let(:url) { "/admin/v1/licenses/#{license.id}" }
 
     before(:each) { patch url, headers: auth_header(user) }
 
     include_examples "forbidden access"
   end
 
-  context "DELETE /products/:id" do
-    let!(:product) { create(:product) }
-    let(:url) { "/admin/v1/products/#{product.id}" }
+  context "DELETE /licenses/:id" do
+    let!(:license) { create(:license) }
+    let(:url) { "/admin/v1/licenses/#{license.id}" }
 
     before(:each) { delete url, headers: auth_header(user) }
 
