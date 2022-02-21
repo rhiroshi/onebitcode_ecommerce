3bfa48931e62251250991e7fc292c2d068293d67
Rafael H. F. Shintani <rhiroshi14@hotmail.com>
2022-02-21 12:41:58 -0300

    Refatorando page p2

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -3,7 +3,8 @@ module Admin::V1
     before_action :load_category, only: [:update, :destroy, :show]
 
     def index
-      @categories = load_categories
+      @loading_service = Admin::ModelLoadingService.new(Category.all, searchable_params)
+      @loading_service.call
     end
 
     def show
@@ -32,9 +33,8 @@ module Admin::V1
       @category = Category.find(params[:id])
     end
 
-    def load_categories
-      permitted = params.permit({search: :name}, {order: {}}, :page, :length)
-      Admin::ModelLoadingService.new(Category.all, permitted).call
+    def searchable_params
+      params.permit({search: :name}, {order: {}}, :page, :length)
     end
 
     def category_params
