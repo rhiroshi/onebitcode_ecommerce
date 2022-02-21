a6facb1f4338440f2e0079421203c76eafdc112c
Rafael H. F. Shintani <rhiroshi14@hotmail.com>
2022-02-21 12:37:45 -0300

    Refatorando pagination p1

--- app/services/admin/model_loading_service.rb
+++ app/services/admin/model_loading_service.rb
@@ -1,15 +1,28 @@
 module Admin
   class ModelLoadingService
+    attribute_reader :records, :pagination
+
     def initialize(searchable_model, params = {})
       @searchable_model = searchable_model
-      @params = params
-      @params ||= {}
+      @params = params || {}
+      @records = []
+      @pagination = { page: @params[:page].to_i, length: params[:length].to_i }
     end
 
     def call
-      @searchable_model.search(@params[:search])
-                       .order(@params[:order].to_h)
-                       .paginate((@params[:page] || 1).to_i, (@params[:length] || 10).to_i)
+      fix_pagination_values
+      filtered = @searchable_model.search_by_name(@params.dig(:search, :nome))
+
+      @records = filtered.order(@params[:order].to_h).paginate(@pagination[:page], @pagination[:length])
+      total_pages = (filtered.count / @pagination.length).ceil
+      @pagination.merge!(total: filtered.count, total_pages: total_pages)
+    end
+
+    private
+
+    def fix_pagination_values
+      @pagination[:page] = @searchable_model.model::DEFAULT_PAGE if @pagination[:page] <= 0
+      @pagination[:length] = @searchable_model.model::MAX_PER_PAGE if @pagination[:length] <= 0
     end
   end
 end
